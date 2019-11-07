e2095f2c2c80c805dca137444cd1fc8fe6e50c08
Edgar Cheverier <edgar.aguilar@devex.com>
2019-11-07 23:14:01 +0100

    Part2 2.9: The Phonebook Step4

--- part2/2.6-2.11_the_phonebook/src/App.js
+++ part2/2.6-2.11_the_phonebook/src/App.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 
 function App() {
-
-  const [persons, setPersons] = useState([{name: 'Edgar Cheverier', number: 123456}]);
+  const initialPersonsState = [
+    {name: 'Edgar Cheverier', number: 45625},
+    {name: 'Eileen Juergens', number: 1235},
+    {name: 'Antoinete Powers', number: 85466},
+    {name: 'Andrew powers', number: 9452},
+    {name: 'Donovan powers', number: 521546},
+  ];
+  const [persons, setPersons] = useState(initialPersonsState);
+  const [filteredPersons, setFilteredPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
+
+  const handleFilter = () => (e) => {
+    const value = e.target.value
+    setNewFilter(value);
+    const newFilteredPersons =
+      persons.filter(person => person.name.toLowerCase().includes(value.toLowerCase()));
+    setFilteredPersons(newFilteredPersons);
+  };
 
   const verifyName = () =>
     persons.some(person => person.name.toLowerCase() === newName.toLowerCase());
@@ -21,12 +37,23 @@ function App() {
     }
   };
 
-  const displayNumbers = () =>
-    persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+  const displayNumbers = () => {
+    if (newFilter) {
+      if (filteredPersons.length) {
+        return filteredPersons.map(filteredPerson =>
+          <li key={filteredPerson.name}>{filteredPerson.name} {filteredPerson.number}</li>)
+      }
+      return <li>no results</li>
+    }
+    return persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilter()} />
+      </div>
       <form onSubmit={handleSubmitPerson}>
         <div>
           name: <input value={newName} onChange={e => setNewName(e.target.value)} />
