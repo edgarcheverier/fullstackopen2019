0100d1d5c78f93d37cef52d68000a13678cea512
Edgar Cheverier <edgar.aguilar@devex.com>
2019-11-10 18:18:37 +0100

    Part2 2.14 Data for countries Step3

--- part2/2.12-2.14_data_for_countries/src/components/DetailCountry.js
+++ part2/2.12-2.14_data_for_countries/src/components/DetailCountry.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const DetailCountry = ({country}) => {
+  const [weather, setWeather] = useState({});
 
   const displayLanguages = () =>
     country.languages.map(language => <li key={language.name}>{language.name}</li>)
+
+  const displayWeather = () => {
+    if (weather.current) {
+      return (
+        <>
+          <p>
+            <strong>temperature: </strong> {weather.current.temperature} Celsius
+          </p>
+          <img src={weather.current.weather_icons} alt={weather.current.weather_icons} style={{width: '100px'}}/>
+          <p>
+            <strong>wind: </strong> {weather.current.wind_speed} kph direction {weather.current.wind_dir}
+          </p>
+        </>
+      )
+    }
+    return <p>An error has occurred when trying getting the {country.capital} weather, try again later.</p>
+  };
+
+  useEffect(() => {
+    const url = `http://api.weatherstack.com/current?access_key=80d19fe10c1845ff2a8ae2b9b00e0e1c&query=${country.capital}`;
+    fetch(url)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return {}
+    })
+    .then(res => {
+      setWeather(res)
+    })
+    setWeather({})
+  }, [country.capital]);
 
   return (
     <div>
@@ -15,6 +48,8 @@ const DetailCountry = ({country}) => {
         {displayLanguages()}
       </ul>
       <img src={country.flag} alt={country.name} style={{width: '150px'}}/>
+      <h1>Weather in {country.capital}</h1>
+      {displayWeather()}
     </div>
   )
 };
